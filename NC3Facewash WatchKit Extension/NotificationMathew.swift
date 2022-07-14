2f3bfb0552e082c3d4d8e33cb34253863c2542ec
rahmatmaftuh <78604097+rahmatmaftuh@users.noreply.github.com>
2022-07-14 14:54:20 +0700

    uploadingfilesmanually

---
+++ NC3Facewash WatchKit Extension/NotificationMathew.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationMathew.swift
+//  NC3Facewash WatchKit Extension
+//
+//  Created by Rahmat Maftuh Ihsan on 14/07/22.
+//
+
+import SwiftUI
+
+struct NotificationMathew: View {
+    var body: some View {
+        Text("Ini teks rahmat yg buat 14.31 14 juli")
+    }
+}
+
+struct NotificationMathew_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationMathew()
+    }
+}
